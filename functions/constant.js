60b30a3772475a8d1f1df7b48c306360b6449696
Kuldeep-Kumar-Sharma <sharman94kuldeep@gmail.com>
2020-11-26 23:46:17 +0530

    updated the code

--- functions/constant.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze({
-    BASE_IMG_URL:"https://firebasestorage.googleapis.com/v0/b/goodbyebot-ykotag.appspot.com/o/",
-    ALPHABETS:"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-});
-
-
