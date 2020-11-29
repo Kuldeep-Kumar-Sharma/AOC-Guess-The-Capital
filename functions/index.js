775859613926efa33e51c3d6aaadc2bd9f748478
Kuldeep-Kumar-Sharma <sharman94kuldeep@gmail.com>
2020-11-29 23:49:21 +0530

    updated the messages

--- functions/index.js
+++ functions/index.js
@@ -2,34 +2,77 @@
 //Initialize libraries
 const { dialogflow } = require("actions-on-google");
 const functions = require("firebase-functions");
+const { BasicCard, Image, Suggestions } = require("actions-on-google");
 
 //Initialize contants
 const COUNTRY_CAPITAL = require("./country-by-capital");
-const { BasicCard, Image, Suggestions } = require("actions-on-google");
-
 const app = dialogflow({ debug: true });
+var answer = "";
+var answer_count = 4;
+var score = 0;
 
+/**
+ *Quiting the app Intent.
+ *INTENT NAME: guess.quit_app
+ *Conversation Output: shows score,cloing conversation.
+ */
 app.intent("guess.quit_app", (conv) => {
-  conv.close(`Have a good day! come back again. Bye!`);
-});
-
-app.intent("guess.fallback", (conv) => {
   conv.close(
-    `Sorry Couldn't get that, can you say it again. In order to exit just say cancel or stop!`
+    showScore() + `Thanks for Playing, Have a good day! come back again. Bye!`
   );
 });
 
+/**
+ *fallback Intent.
+ *INTENT NAME: guess.fallback
+ *Conversation Output: checking the chances,cloing conversation.
+ */
+app.intent("guess.fallback", (conv) => {
+  answer_count = answer_count - 1;
+  if (answer_count > 0) {
+    conv.ask(
+      `Sorry, I Couldn't  get that, you have ${answer_count} more chances to say the anser right!`
+    );
+  } else {
+    conv.close(
+      `Sorry Couldn't get that, can you say it again. In order to exit just say cancel or stop!`
+    );
+  }
+});
+
+/**
+ *getRndInteger
+ * @param {INT} min
+ * @param {INT} max
+ * Generates the random number from minumum and maximum range
+ */
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
-let answer = "";
-let answer_count = 4;
-function genreateRandomCountryQuestion() {
+
+/**
+ * genreateRandomConuntryQuestion()
+ * Generates a random question based on Random Number on Country's capital.
+ */
+function generateRandomCountryQuestion() {
   let randomCountry = getRndInteger(0, 244);
   let country = COUNTRY_CAPITAL[randomCountry].country;
   return `What is the Capital of ` + country;
 }
 
+/**
+ * showScore()
+ * returns the score.
+ */
+function showScore() {
+  return `Your Score is ${score}`;
+}
+
+/*
+ *start Intent.
+ *INTENT NAME: guess.start
+ *Conversation Output: checking the chances,cloing conversation.
+ */
 app.intent("guess.start", (conv) => {
   // [START asdk_js_basic_card]
   if (!conv.screen) {
@@ -43,7 +86,7 @@ app.intent("guess.start", (conv) => {
     `Welcome to Guess The Capital Game !! Let's start by 
     guessing the capital of any country I ask from you. You can go wrong twice only.`
   );
-  conv.ask(genreateRandomCountryQuestion());
+  conv.ask(generateRandomCountryQuestion());
 });
 
 app.intent("guess.capital", (conv, params) => {
@@ -54,6 +97,12 @@ app.intent("guess.capital", (conv, params) => {
         "phone surface in the simulator."
     );
     return;
+  }
+  if (answer_count == 0) {
+    conv.ask(
+      `You have reached the maximum number of Attempts, Your Score is ` +
+        showScore()
+    );
   }
   if (params.capital == answer) {
     conv.ask(`Congratulation's you gave the right answer!`);
